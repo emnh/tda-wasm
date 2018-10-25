4123b009179d782151e495c972cf823fff099bdc
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2018-10-25 14:38:34 +0200

    initial commit. working example

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+all: dist/index.js dist/index.html
+
+clean:
+	rm dist/index.js dist/index.wasm dist/index.html
+
+dist/index.html: src/index.html
+	cp src/index.html dist/index.html
+
+dist/index.js: cpp/main.cpp
+	emcc cpp/main.cpp -std=c++17 -s WASM=1 -s USE_WEBGL2=1 -O3 -o dist/index.js
---
+++ cpp/main.cpp
@@ -0,0 +1,98 @@
+#include <functional>
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <GLES3/gl3.h>
+
+// Shader sources
+const GLchar* vertexSource =
+    "attribute vec4 position;                     \n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  gl_Position = vec4(position.xyz, 1.0);     \n"
+    "}                                            \n";
+const GLchar* fragmentSource =
+    "precision mediump float;\n"
+    "void main()                                  \n"
+    "{                                            \n"
+    "  gl_FragColor[0] = gl_FragCoord.x/640.0;    \n"
+    "  gl_FragColor[1] = gl_FragCoord.y/480.0;    \n"
+    "  gl_FragColor[2] = 0.5;                     \n"
+    "}                                            \n";
+
+// an example of something we will control from the javascript side
+bool background_is_black = true;
+
+// the function called by the javascript code
+extern "C" void EMSCRIPTEN_KEEPALIVE toggle_background_color() { background_is_black = !background_is_black; }
+
+std::function<void()> loop;
+void main_loop() { loop(); }
+
+int main()
+{
+    // Context configurations
+    EmscriptenWebGLContextAttributes attrs;
+    attrs.explicitSwapControl = 0;
+    attrs.depth = 1;
+    attrs.stencil = 1;
+    attrs.antialias = 1;
+    attrs.majorVersion = 2;
+    attrs.minorVersion = 0;
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context;
+    context = emscripten_webgl_create_context("canvas", &attrs);
+    emscripten_webgl_make_context_current(context);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {0.0f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f};
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    loop = [&]
+    {
+        // move a vertex
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // Clear the screen
+        if( background_is_black )
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        else
+            glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle from the 3 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    };
+
+    emscripten_set_main_loop(main_loop, 0, true);
+
+    return EXIT_SUCCESS;
+}
